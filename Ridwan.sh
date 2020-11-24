d2b794ea471341ccbc3ae02d4c831c3dbb33d8d6
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-11-24 15:29:32 +0700

    Add files via upload

---
+++ Ridwan.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#warna
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+#pembuka
+clear
+echo $ku "hello Semuanya Selamat Datang di Chhanel Ridwan Kechil"
+sleep 3
+echo $bi "mari kita kembangkan channel ini untuk menjadi yg lebih baik"
+sleep 2
+echo $or "jika ada saran atau kritikan silahkan komentar ke instagram saya"
+sleep 1
+echo $me "kalau mau requests script boleh di kirim ke Instagram "
+sleep 1
+echo $ku "Nama Instagram :: Ridwan Kechil "
+sleep 1
+echo $me "UserName :: Ridwan58aja"
+
+#Selesai
